691f623cc9ebcd625639766f073aae08ea56b85a
glorius <glorius@zohomail.eu>
2024-02-10 15:04:29 +0300

    add inner-right

--- render.js
+++ render.js
@@ -1,8 +1,10 @@
 let buttons = document.getElementsByClassName("btn");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
-        let activeButton = document.querySelector(".btn-active");
-        activeButton.classList.remove("btn-active");
-        this.classList.add("btn-active");
+        if (!this.classList.contains("info-btn")) {
+            let activeButton = document.querySelector(".btn-active");
+            activeButton.classList.remove("btn-active");
+            this.classList.add("btn-active");
+        }
     });
 }
